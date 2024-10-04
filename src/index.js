f92eb4072c2678865b5b51b4671cfb43a2708e10
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-10-04 22:32:50 +0530

    New updates

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const { getBalance, getRecentBlockhash } = require('./rpcClient');
 const run = async () => {
   const publicKey = 'YourPublicKeyHere';  
 
-  try {
-    const balance = await getBalance(publicKey);
+try {
+  const balance = await getBalance(publicKey);
     console.log(`Balance for ${publicKey}: ${balance} lamports`);
 
     const blockhash = await getRecentBlockhash();
