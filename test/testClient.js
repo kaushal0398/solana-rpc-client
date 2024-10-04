d209623feafb7d4ff9d5f5adcf8b7c4f8db25766
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-10-04 22:31:31 +0530

    New updates

--- test/testClient.js
+++ test/testClient.js
@@ -1,17 +1,17 @@
 const { getBalance, getRecentBlockhash } = require('../src/rpcClient');
 
 const testRpcClient = async () => {
-  const publicKey = '8Dp2SYMeviWy5vhrkTJuKqRh8J1bAZRw68vBp7eSbu1'; 
+const publicKey = '8Dp2SYMeviWy5vhrkTJuKqRh8J1bAZRw68vBp7eSbu1'; 
 
-  try {
+try {
     const balance = await getBalance(publicKey);
     console.log(`Balance for ${publicKey}: ${balance} lamports`);
 
     const blockhash = await getRecentBlockhash();
     console.log('Recent blockhash:', blockhash);
-  } catch (error) {
+} catch (error) {
     console.error('Test failed:', error.message);
-  }
+ }
 };
 
 testRpcClient();
